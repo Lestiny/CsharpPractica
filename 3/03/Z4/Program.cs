188b8557cef4b68c300d79c02f3f944299faab1b
Lestiny <engeliia.ludcefer@gmail.com>
2026-03-19 20:25:53 +0300

    02Practica

---
+++ 3/03/Z4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] house = new int[12, 4];
+        Random rnd = new Random();
+
+        for (int i = 0; i < 12; i++)
+            for (int j = 0; j < 4; j++)
+                house[i, j] = rnd.Next(1,6);
+
+        Console.WriteLine("Количество жильцов(12 этажей по 4 квартиры):\n");
+
+        for (int i = 0; i < 12; i++)
+        {
+            Console.Write($"Этаж {i + 1,2}: ");
+            for (int j = 0; j < 4; j++)
+                Console.Write(house[i, j] + " ");
+            Console.WriteLine();
+        }
+
+        int maxFamily = 0;
+
+        for (int i = 2; i <= 3; i++)
+            for (int j = 0; j < 4; j++)
+                if (house[i, j] > maxFamily)
+                    maxFamily = house[i, j];
+
+        Console.WriteLine($"\nСамая большая семья на 3-4 этажах: {maxFamily}");
+    }
+}
---
+++ 3/03/Z8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите первую строку: ");
+        string s1 = Console.ReadLine();
+
+        Console.Write("Введите вторую строку: ");
+        string s2 = Console.ReadLine();
+
+        bool equal = CompareStrings(s1, s2);
+
+        Console.WriteLine(equal ? "Строки равны" : "Строки не равны");
+    }
+
+    static bool CompareStrings(string a, string b)
+    {
+        string s1 = a.Replace(" ", "").ToLower();
+        string s2 = b.Replace(" ", "").ToLower();
+
+        return s1 == s2;
+    }
+}
