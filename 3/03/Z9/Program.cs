5dd53aaff54b9738f2e6ad77bbd682e9df7bec72
Lestiny <engeliia.ludcefer@gmail.com>
2026-03-20 14:02:29 +0300

    Practica03

---
+++ 3/03/Z9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите строку: ");
+        string input = Console.ReadLine();
+
+        string result = RemoveNonAlphanumeric(input);
+
+        Console.WriteLine("Результат: " + result);
+    }
+
+    static string RemoveNonAlphanumeric(string s)
+    {
+        string result = "";
+
+        foreach (char c in s)
+            if (char.IsLetterOrDigit(c))
+                result += c;
+
+        return result;
+    }
+}
